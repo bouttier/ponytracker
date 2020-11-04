f5d9ab6e13b554b77794fbe4db788c94f6808eae
Tom Pillot <tom.pillot@pm.me>
2020-11-04 18:54:42 +0100

    fix celery task not registered

--- ponytracker/celeryapp.py
+++ ponytracker/celeryapp.py
@@ -2,6 +2,7 @@ import os
 
 from celery import Celery
 
+from django.apps import apps
 from django.conf import settings
 
 
@@ -10,7 +11,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ponytracker.settings')
 app = Celery('ponytracker')
 
 app.config_from_object('django.conf:settings')
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+app.autodiscover_tasks(lambda: [n.name for n in apps.get_app_configs()])
 
 
 @app.task(bind=True)
